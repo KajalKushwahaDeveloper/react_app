fe861873a3093cb1f6887a66313998efe202114f
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-06-29 16:33:18 +0530

    map has been created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import LoginPage from "./login_page";
 import Home from "./home";
 import GPS from "./gps";
 import Logout from "./logout.js";
+import Settings from "./settings.js";
 import ResetPasswordPage from "./resetPassword";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/home" element={<Home/>} />
           <Route path="/gps" element={<GPS/>} />
+          <Route path="/settings" element={<Settings/>} />
           <Route path="/logout" element={<Logout/>} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
     </Routes>
---
+++ src/settings.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./scss/map.scss";
+import "./scss/login.scss";
+import GoogleMapReact from "google-map-react";
+import LocationPin from "./components/location_pin";
+
+const Settings = ({ location, zoomLevel }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+  return (
+    <>
+      <div className="map">
+        {/* <h2 className="map-h2">Come Visit Us At Our Campus</h2> */}
+        <input
+          type="text"
+          className="content_input"
+          placeholder="search here"
+          value={search}
+          onChange={handleChange}
+        />
+        <div className="google-map">
+          <GoogleMapReact
+            bootstrapURLKeys={{ 
+                key: "" 
+            }}
+            defaultCenter={location}
+            defaultZoom={zoomLevel}
+          >
+            <LocationPin
+              lat={location.lat}
+              lng={location.lng}
+              text={location.address}
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Settings;
