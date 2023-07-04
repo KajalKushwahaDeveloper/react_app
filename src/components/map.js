a38d7c0a856b90de3ee9cafeba203bcc8cb36697
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-07-04 11:19:08 +0530

    Added checkbox in gps_page

--- src/components/map.js
+++ src/components/map.js
@@ -1,73 +1,73 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
-import "../scss/map.scss"
+// import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+// import { useMemo } from "react";
+// import "../scss/map.scss"
 
-const Map = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+// const Map = () => {
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+//   });
+//   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
 
-  return (
-    <div className="map">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          zoom={10}
-        />
-      )}
-    </div>
-  );
-};
-
-export default Map;
-
-// import React, { useState } from "react";
-// import "../scss/map.scss";
-// import "../scss/login.scss";
-// import GoogleMapReact from "google-map-react";
-// import LocationPin from "./location_pin";
-
-// const Map = ({ location, zoomLevel }) => {
-//   const [search, setSearch] = useState("");
-
-//   const handleChange = (e) => {
-//     e.preventDefault();
-//     setSearch(e.target.value);
-//     console.log(e.target.value);
-//   };
 //   return (
-//     <>
-//       <div className="map">
-//         {/* <h2 className="map-h2">Come Visit Us At Our Campus</h2> */}
-//         <input
-//           type="text"
-//           className="content_input"
-//           placeholder="search here"
-//           value={search}
-//           onChange={handleChange}
+//     <div className="map">
+//       {!isLoaded ? (
+//         <h1>Loading...</h1>
+//       ) : (
+//         <GoogleMap
+//           mapContainerClassName="map-container"
+//           center={center}
+//           zoom={10}
 //         />
-//         <div className="google-map">
-//           <GoogleMapReact
-//             bootstrapURLKeys={{ 
-//                 key: "" 
-//             }}
-//             defaultCenter={location}
-//             defaultZoom={zoomLevel}
-//           >
-//             <LocationPin
-//               lat={location.lat}
-//               lng={location.lng}
-//               text={location.address}
-//             />
-//           </GoogleMapReact>
-//         </div>
-//       </div>
-//     </>
+//       )}
+//     </div>
 //   );
 // };
 
 // export default Map;
+
+import React, { useState } from "react";
+import "../scss/map.scss";
+import "../scss/login.scss";
+import GoogleMapReact from "google-map-react";
+import LocationPin from "./location_pin";
+
+const Map = ({ location, zoomLevel }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+  return (
+    <>
+      <div className="map">
+        {/* <h2 className="map-h2">Come Visit Us At Our Campus</h2> */}
+        <input
+          type="text"
+          className="content_input"
+          placeholder="search here"
+          value={search}
+          onChange={handleChange}
+        />
+        <div className="google-map">
+          <GoogleMapReact
+            bootstrapURLKeys={{ 
+                key: "AIzaSyAOJ2QPH1vPWF7wXqdHMGFR54Vzlb13M1E" 
+            }}
+            defaultCenter={location}
+            defaultZoom={zoomLevel}
+          >
+            <LocationPin
+              lat={location.lat}
+              lng={location.lng}
+              text={location.address}
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Map;
