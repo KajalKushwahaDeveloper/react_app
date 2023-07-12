f4679e8f8f4b6717e2cf335f24649904c9d6e428
saurabh <saurabhshrestha87@gmail.com>
2023-07-12 12:35:59 +0530

    Map Changes, markers for stop, emulators, etc

--- src/components/location/Map.js
+++ src/components/location/Map.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import {
   GoogleMap,
   withScriptjs,
   withGoogleMap,
   Marker,
   Polyline,
+  InfoWindow,
 } from "react-google-maps";
 import useFetch from "../hooks/useFetch";
 import Button from "@mui/material/Button";
@@ -12,28 +13,34 @@ import Card from "@mui/material/Card";
 import GpsTable from "../gps_page_table";
 import CurrentLocation from "../current_location";
 import CreateTripTable from "../create_trip_table";
-import { TRIP_URL } from "../../constants";
+import { TRIP_STOPS_URL, TRIP_URL, EMULATOR_URL } from "../../constants";
 
 const Map = ({ showToast }) => {
-  const defaultLat = 37.7749; // Default latitude
-  const defaultLng = -122.4194; // Default longitude
-
   const [progress, setProgress] = useState(null);
   const [selectedEmId, setSelectedEmId] = useState(1);
   const [createTrip, setCreateTrip] = useState(false);
 
   const [pathsRoute, setPathsRoute] = useState(null);
-  const [centerPathLat, setCenterPathLat] = useState(null);
-  const [centerPathLng, setCenterPathLng] = useState(null);
+
+  const defaultLat = 37.7749; // Default latitude
+  const defaultLng = -122.4194; // Default longitude
+
+  const [center, setCenter] = useState({
+    lat: defaultLat,
+    lng: defaultLng,
+  });
+
+  const mapRef = useRef(null);
 
   const handleCreateTripButton = () => {
     setCreateTrip(true);
   };
 
   const { data: paths } = useFetch(TRIP_URL + `/${selectedEmId}`);
-  const { data: stops } = useFetch(
-    "https://61a4a0604c822c0017041d33.mockapi.io/shuttle/v1/stops"
-  );
+  const { data: stops } = useFetch(TRIP_STOPS_URL + `/${selectedEmId}`);
+  const { data: emulators } = useFetch(EMULATOR_URL);
+  const [selectedStop, setSelectedStop] = useState(null);
+
   const velocity = 27; // 100km per hour
   let initialDate;
   let interval = null;
@@ -50,8 +57,7 @@ const Map = ({ showToast }) => {
     }
     console.log("GOT PATH, CALCULATING ROUTE! paths : ", paths);
     const center = parseInt(paths.length / 2);
-    setCenterPathLat(paths[center].lat);
-    setCenterPathLng(paths[center + 5].lng);
+    setCenter({ lat: paths[center].lat, lng: paths[center + 5].lng });
     calculatePath();
     return () => {
       interval && window.clearInterval(interval);
@@ -71,6 +77,12 @@ const Map = ({ showToast }) => {
     console.log("CHANGED pathsRoute : ", pathsRoute);
   }, [pathsRoute]);
 
+  useEffect(() => {
+    if (emulators === null) {
+      console.log("CHANGED emulators : Null emulators");
+    }
+    console.log("CHANGED emulators : ", emulators);
+  }, [emulators]);
 
   const moveObject = (pathsRoute) => {
     const distance = getDistance();
@@ -122,35 +134,49 @@ const Map = ({ showToast }) => {
   };
 
   const calculatePath = () => {
-    setPathsRoute(paths.map((coordinates, i, array) => {
-      if (i === 0) {
-        return { ...coordinates, distance: 0 }; // it begins here!
-      }
-      const { lat: lat1, lng: lng1 } = coordinates;
-      const latLong1 = new window.google.maps.LatLng(lat1, lng1);
-
-      const { lat: lat2, lng: lng2 } = array[0];
-      const latLong2 = new window.google.maps.LatLng(lat2, lng2);
-
-      // in meters:
-      const distance =
-        window.google.maps.geometry.spherical.computeDistanceBetween(
-          latLong1,
-          latLong2
+    if (mapRef.current === null || paths === null || paths.length === 0) {
+      return;
+    }
+    const bounds = new window.google.maps.LatLngBounds();
+    setPathsRoute(
+      paths.map((coordinates, i, array) => {
+        bounds.extend(
+          new window.google.maps.LatLng(coordinates.lat, coordinates.lng)
         );
+        if (i === 0) {
+          return { ...coordinates, distance: 0 }; // it begins here!
+        }
+        const { lat: lat1, lng: lng1 } = coordinates;
+        const latLong1 = new window.google.maps.LatLng(lat1, lng1);
 
-      return { ...coordinates, distance };
-    }));
+        const { lat: lat2, lng: lng2 } = array[0];
+        const latLong2 = new window.google.maps.LatLng(lat2, lng2);
+
+        // in meters:
+        const distance =
+          window.google.maps.geometry.spherical.computeDistanceBetween(
+            latLong1,
+            latLong2
+          );
+
+        return { ...coordinates, distance };
+      })
+    );
+
+    mapRef.current.fitBounds(bounds);
   };
-  
-  const startSimulation = useCallback((pathsRoute) => {
+
+  const startSimulation = useCallback(
+    (pathsRoute) => {
       console.log("HELLO !!! PATHS : ", paths);
       console.log("HELLO !!! pathsRoute : ", pathsRoute);
       initialDate = new Date();
       setProgress(null);
       initialDate = new Date();
       interval = window.setInterval(moveObject(pathsRoute), 1000);
-  }, [interval, initialDate]);
+    },
+    [interval, initialDate]
+  );
 
   const mapUpdate = () => {
     const distance = getDistance();
@@ -194,11 +220,38 @@ const Map = ({ showToast }) => {
     }
   };
 
+  const handleMarkerClick = (stop) => {
+    setSelectedStop(stop);
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedStop(null);
+  };
+
+  const handleEmulatorMarkerClick = (emulator) => {
+    setSelectedEmId(emulator.id);
+  };
+
+  const handleEmulatorMarkerDragEnd = (emulator, event) => {
+    if (emulator.startLat === null) {
+      const { latLng } = event;
+      const lat = latLng.lat();
+      const lng = latLng.lng();
+      console.log(`New Latitude: ${lat}, New Longitude: ${lng}`);
+    }
+  };
+
+  const handleEmulatorMarkerDragStart = (emulator) => {
+    if (emulator.startLat) {
+      showToast("Emulator has a Trip, Can't drag", "error"); // Replace with your own showToast method
+    }
+  };
+  
+
   return (
     <Card variant="outlined">
       <div className="btnCont">
-        
-        <Button variant="contained" onClick={()=>startSimulation(pathsRoute)}>
+        <Button variant="contained" onClick={() => startSimulation(pathsRoute)}>
           Start Simulation
         </Button>
       </div>
@@ -227,13 +280,8 @@ const Map = ({ showToast }) => {
       {/* table */}
 
       <div className="gMapCont">
-        <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{
-            lat: centerPathLat || defaultLat,
-            lng: centerPathLng || defaultLng,
-          }}
-        >
+        {console.log("center changed : ", center)}
+        <GoogleMap ref={mapRef} defaultZoom={7} center={center}>
           {pathsRoute != null && (
             <Polyline
               path={pathsRoute}
@@ -245,20 +293,60 @@ const Map = ({ showToast }) => {
               }}
             />
           )}
-
+          {console.log("stops : ", stops)}
           {stops != null &&
-            stops.data != null &&
-            stops.data.map((stop, index) => (
-              <Marker
-                key={index}
-                position={{
-                  lat: stop.lat,
-                  lng: stop.lng,
-                }}
-                title={stop.id}
-                label={`${index + 1}`}
-              />
+            stops.map((stop, index) => (
+              <React.Fragment key={index}>
+                <Marker
+                  position={{
+                    lat: stop.lat,
+                    lng: stop.lng,
+                  }}
+                  title={"Stop" + stop.id}
+                  label={`S${index + 1}`}
+                  onClick={() => handleMarkerClick(stop)}
+                />
+                {stop.tripPoints && stop.tripPoints.length > 0 && (
+                  <Polyline
+                    path={stop.tripPoints}
+                    options={{
+                      strokeColor: "#FF2200",
+                      strokeWeight: 6,
+                      strokeOpacity: 0.6,
+                      defaultVisible: true,
+                    }}
+                  />
+                )}
+              </React.Fragment>
             ))}
+          {console.log("selectedStop : ", selectedStop)}
+          {selectedStop && (
+            <InfoWindow
+              position={{ lat: selectedStop.lat, lng: selectedStop.lng }}
+              onCloseClick={handleInfoWindowClose}
+            >
+              <div style={{ width : "auto" }}>
+                <h3 style={{ color: "black" }}>Stop Address:</h3>
+                <p style={{ color: "black" }}>
+                  {selectedStop.address.map((addressItem, index) => (
+                    <React.Fragment key={index}>
+                      {index > 0 && ", "}
+                      {addressItem.long_name}
+                    </React.Fragment>
+                  ))}
+                </p>
+                <h3 style={{ color: "black" }}>Nearest Gas Station:</h3>
+                <p style={{ color: "black" }}>
+                  {selectedStop.gasStation.map((gasStationAddressItem, index) => (
+                    <React.Fragment key={index}>
+                      {index > 0 && ", "}
+                      {gasStationAddressItem.long_name}
+                    </React.Fragment>
+                  ))}
+                </p>
+              </div>
+            </InfoWindow>
+          )}
 
           {progress && (
             <>
@@ -267,6 +355,24 @@ const Map = ({ showToast }) => {
               <Marker icon={icon1} position={progress[progress.length - 1]} />
             </>
           )}
+
+          {emulators &&
+            emulators.map((emulator, index) => (
+              <React.Fragment key={index}>
+                <Marker
+                  position={{
+                    lat: emulator.latitude,
+                    lng: emulator.longitude,
+                  }}
+                  title={`Emulator ${emulator.id}`}
+                  label={`S${emulator.id}`}
+                  onClick={() => handleEmulatorMarkerClick(emulator)}
+                  draggable={!emulator.startLat}
+                  onDragStart={() => handleEmulatorMarkerDragStart(emulator)}
+                  onDragEnd={(event) => handleEmulatorMarkerDragEnd(emulator, event)}
+                />
+              </React.Fragment>
+            ))}
         </GoogleMap>
       </div>
     </Card>
