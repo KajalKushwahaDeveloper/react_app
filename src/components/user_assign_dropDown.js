2e7da340bd346a3ac614f10a6513c77c7fdb15c4
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-07-21 11:38:20 +0530

    Add button functionality

--- src/components/user_assign_dropDown.js
+++ src/components/user_assign_dropDown.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
@@ -6,7 +6,7 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Dialog from "@mui/material/Dialog";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -15,46 +15,43 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { USER_URL, USER_ASSIGN_EMULATOR_URL } from "../constants";
 
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
 
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
 
-  
-  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    "& .MuiDialogContent-root": {
-      padding: theme.spacing(2),
-    },
-    "& .MuiDialogActions-root": {
-      padding: theme.spacing(1),
-    },
-  }));
-  
-  function BootstrapDialogTitle(props) {
-    const { children, onClose, ...other } = props;
-  
-    return (
-      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-        {children}
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            onClick={onClose}
-            sx={{
-              position: "absolute",
-              right: 0,
-              top: 0,
-              color: (theme) => theme.palette.blue,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </DialogTitle>
-    );
-  }
-  
-  BootstrapDialogTitle.propTypes = {
-    children: PropTypes.node,
-    onClose: PropTypes.func.isRequired,
-  };
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 0,
+            top: 0,
+            color: (theme) => theme.palette.blue,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
+
+BootstrapDialogTitle.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -66,22 +63,8 @@ const MenuProps = {
   },
 };
 
-const names = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder",
-];
 
-
-
-export default function UserAssignDropDown(props) {
+export default function UserAssignDropDown(showToast,handleOpen, close, emulatorToAssignUser , handleAssignedUserToEmulator ) {
   const theme = useTheme();
   const [personName, setPersonName] = React.useState([]);
   const [open, setOpen] = React.useState(false);
@@ -89,6 +72,30 @@ export default function UserAssignDropDown(props) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+
+  const handleUserSelect = async (userId) => {
+    try {
+      console.log(emulatorToAssignUser.id);
+      console.log(userId);
+      const { success,error,data } = await setUserToEmulator(emulatorToAssignUser.id, userId);
+
+      if (success) {
+        console.log("User added successfully");
+        if(emulatorToAssignUser != null){
+          handleAssignedUserToEmulator(success, error, data )
+        } else {
+          handleAssignedUserToEmulator(success, error, null )
+        }
+        showToast("User Added", "success"); 
+      } else {
+        showToast(error || "Failed to add user", "error"); // Call the showToast method with two arguments
+        setError(error || "Failed to add user"); // Display appropriate error message
+      }
+    } catch (error) {
+      console.log("Error occurred while adding user:", error);
+      setError("An error occurred while adding user"); // Display a generic error message
+    }
+  };
 
   // Fetch data from API
   const fetchUsers = async () => {
@@ -105,15 +112,13 @@ export default function UserAssignDropDown(props) {
         return { success: false, error: "Invalid credentials" };
       } else {
         const responseData = await response.text();
-       
 
         const deserializedData = JSON.parse(responseData);
         console.log("response:::", deserializedData);
 
-
         setUsers(deserializedData);
         setLoading(false);
-        return { success: true, error: null};
+        return { success: true, error: null };
       }
     } catch (error) {
       console.log("User Data Error: " + error);
@@ -121,7 +126,6 @@ export default function UserAssignDropDown(props) {
       setLoading(false);
     }
   };
-
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -138,61 +142,65 @@ export default function UserAssignDropDown(props) {
     // setPersonName(
     //   typeof value === "string" ? value.split(",") : value
     // );
-    setPersonName(value)
+    setPersonName(value);
   };
   useEffect(async () => {
     // setLoading(true);
     const userData = await fetchUsers();
-    console.log("userData111", userData)
-    
-  }, [props.open]);
+    console.log("userData111", userData);
+  }, [open]);
   return (
     <div>
-    
-      <BootstrapDialog 
-        onClose={props.close}
+      <BootstrapDialog
+        onClose={close}
         aria-labelledby="customized-dialog-title"
-        open={props.open}
+        open={open}
       >
         <BootstrapDialogTitle
           style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "space-between",
-            fontWeight:"100px",
+            fontWeight: "100px",
           }}
           id="customized-dialog-title"
-          onClose={props.close}
+          onClose={close}
         >
           Select User:
         </BootstrapDialogTitle>
         <DialogContent dividers>
-
-        <FormControl sx={{ m: 1, width: 300 , margin:"2rem"}}>
-          <InputLabel id="demo-multiple-name-label" style={{borderRadius:"2rem"}}>Users</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-         
-            value={personName}
-            onChange={handleChange}
-            input={<OutlinedInput label="Name" />}
-            MenuProps={MenuProps}
-          >{
-            console.log("users23:", users)
-          }
-            {users?.map((name) => (
-              <MenuItem
-                key={name}
-                value={name}
-               
+          <FormControl sx={{ m: 1, width: 300, margin: "2rem" }}>
+            <InputLabel
+              id="demo-multiple-name-label"
+              style={{ borderRadius: "2rem" }}
+            >
+              Users
+            </InputLabel>
+            <Select
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              value={personName}
+              onChange={handleChange}
+              input={<OutlinedInput label="Name" />}
+              MenuProps={MenuProps}
+            >
+              {console.log("users23:", users)}
+              {users?.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name.lastModifiedBy}
+                </MenuItem>
+              ))}
+            </Select>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button
+                variant="contained"
+                style={{ width: "2rem", marginTop: "2em" }}
+                onClick={() => handleUserSelect(user.id)}
               >
-                {name.lastModifiedBy}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button variant="contained">Add</Button>
-        </FormControl>
+                Add
+              </Button>
+            </div>
+          </FormControl>
         </DialogContent>
       </BootstrapDialog>
     </div>
