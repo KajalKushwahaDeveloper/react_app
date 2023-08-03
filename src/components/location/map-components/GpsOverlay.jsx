ea919d0cb29fa1e25957083bb513a1be6384b9cd
abhishek <abhishekdiwedi8859@gmail.com>
2023-08-03 18:27:28 +0530

    Tooltip, Font Size Change

--- src/components/location/map-components/GpsOverlay.jsx
+++ src/components/location/map-components/GpsOverlay.jsx
@@ -1,7 +1,6 @@
 // GpsOverlay.jsx
 import React from "react";
 import GpsTable from "./gps_page_table";
-import CurrentLocation from "./current_location";
 import AddressTable from "./address_table";
 
 const GpsOverlay = ({ showToast, emulators, setSelectedEmId , tripData}) => {
@@ -9,7 +8,6 @@ const GpsOverlay = ({ showToast, emulators, setSelectedEmId , tripData}) => {
   return (
     <div className="gps_overlay">
       <GpsTable showToast={showToast} setSelectedEmId={setSelectedEmId} data = {emulators}/>
-      <CurrentLocation />
       <AddressTable tripData={tripData}/>
       
     </div>
