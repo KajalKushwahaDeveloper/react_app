b23bf1a85067cd638fc2a45879cdc8ce0b82fcb2
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2023-08-18 11:59:18 +0530

    login page ui changes

--- src/login_page.js
+++ src/login_page.js
@@ -42,9 +42,9 @@ const LoginPage = () => {
           const token = data.token;
           localStorage.setItem("token", token);
           console.log("Login successful");
-           navigate("/home"); // Redirect to the home page
+          navigate("/home"); // Redirect to the home page
         } else {
-          setResponseError( "Invalid credentials"); // Display appropriate error message
+          setResponseError(error || "Invalid credentials"); // Display appropriate error message
         }
       } catch (error) {
         console.log("Error occurred during login:", error);
@@ -55,49 +55,86 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="main_div">
-        <div className="left">
-          <div className="image">
-            <img src="images/logo2.png" />
-            <hr className="hr"></hr>
-            <p className="para">
-              Vivamus at dui consequat, dapibus tellus vitae
-            </p>
-          </div>
-        </div>
-
-        <div className="right">
-          <div className="map_img">
-            <img src="images/map.png" />
-            <div className="center_div">
-              <div className="content">
+      <div className="authScreen">
+        <div className="container-fluid h-100">
+          <div class="row h-100">
+            <div class="col-md-6 centerMid d-none d-md-flex" style={{  
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+             }}>
+              <div class="Left_content">
+                <img src="images/logo2.png" />
+                <hr className="hr"></hr>
+                <p className="para">
+                  Vivamus at dui consequat, dapibus tellus vitae
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 formBox bg-light  centerMid flex-column" style={{
+                 display: "flex",
+                 alignItems: "center",
+                 justifyContent: "center",   
+            }}>
+              <div className="authCard shadow">
+                <div class="Left_content d-block d-md-none">
+                  <img src="images/logo2.png" />
+                  <p className="para">
+                    Vivamus at dui consequat, dapibus tellus vitae
+                  </p>
+                </div>
                 <form onSubmit={handleSubmit}>
-                  <h1>Log in</h1>
-                  <input
-                    type="email"
-                    id="content_input"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={handleEmailChange}
-                  />
-                  <input
-                    type="password"
-                    id="content_input"
-                    name="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                  />
-                  <button className="login_button" type="submit">
-                    Login
-                  </button>
-                  {passwordError && <p className="error" style={{margin:"0"}}>{passwordError}</p>}
-                  {emailError && <p className="error"style={{margin:"0"}}>{emailError}</p>}
-                  {responseError && <p className="error"style={{margin:"0"}}>{responseError}</p>}
+                  <h1>Log in </h1>
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="inputField">
+                        <input
+                          type="email"
+                          id="content_input"
+                          name="email"
+                          placeholder="Enter your email"
+                          value={email}
+                          onChange={handleEmailChange}
+                          className="form-control"
+                        />
+                        
+                      </div>
+                    </div>
+                    <div className="col-12" style={{marginBottom:"1rem"}}>
+                      <div className="inputField">
+                        <input
+                          type="password"
+                          id="content_input"
+                          name="password"
+                          placeholder="Enter your password"
+                          value={password}
+                          onChange={handlePasswordChange}
+                          className="form-control"
+                        />
+                        
+                      </div>
+                    </div>
+                    <div className="col-12 mt-3 btns" style={{margin:"2rem"}}>
+                      <button className="btn btn-main" type="submit" style={{background:"#007dc6", color:"white",width:"6rem"}}>
+                        Login
+                      </button>
+                    </div>
+                    {emailError && <p className="error">{emailError}</p>}
+                    {passwordError && (
+                          <p className="error">{passwordError}</p>
+                        )}
+                    <div className="col-12 mt-3 btns">
+                      {responseError && (
+                        <p className="error">Invalid Ceredentials</p>
+                      )}
+                    
+                    </div>
+                  </div>
                 </form>
               </div>
             </div>
+            <img src="images/map-location.svg" alt="" className="gps" />
+            <img src="images/middleImg.svg" alt="" className="middle" />
           </div>
         </div>
       </div>
