1607a26079352758bfb828603a22dceecfd74d70
saurabh <shresthasaurabh86@gmail.com>
2023-10-10 16:09:03 +0530

    change mms localhost to Base URL

--- src/components/location/map-components/Phone/sms/http-common.js
+++ src/components/location/map-components/Phone/sms/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BASE_URL }from "../../../../../constants";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   headers: {
     "Content-type": "application/json"
   }
