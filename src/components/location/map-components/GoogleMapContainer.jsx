f6cfd8eb725b002be3dd84ab011c14bd35615499
saurabh <shresthasaurabh86@gmail.com>
2023-11-20 18:46:59 +0530

    marker icons

--- src/components/location/map-components/GoogleMapContainer.jsx
+++ src/components/location/map-components/GoogleMapContainer.jsx
@@ -276,9 +276,9 @@ const GoogleMapContainer = ({
             position={{ lat: startLat, lng: startLng }}
             icon={{
               url: "images/start_location.png",
-              scaledSize: new window.google.maps.Size(10, 10),
+              scaledSize: new window.google.maps.Size(30, 30),
               anchor: new window.google.maps.Point(15, 15),
-              scale: 0.3,
+              scale: 1,
             }}
           />
         )}
@@ -287,10 +287,10 @@ const GoogleMapContainer = ({
           <Marker
             position={{ lat: endLat, lng: endLng }}
             icon={{
-              url: "images/start_location.png",
-              scaledSize: new window.google.maps.Size(10, 10),
+              url: "images/stop_location.png",
+              scaledSize: new window.google.maps.Size(30, 30),
               anchor: new window.google.maps.Point(15, 15),
-              scale: 0.3,
+              scale: 1,
             }}
           />
         )}
