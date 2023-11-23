913dc71ec0ff895c994190f20be3c086217ed4fc
AkshayBishtFtechiz <akshay.bisht1@ftechiz.com>
2023-11-23 16:28:31 +0530

    Designed Table Header

--- src/gps.js
+++ src/gps.js
@@ -1,6 +1,8 @@
 import "./scss/map.scss";
 import { ToastContainer, toast } from "react-toastify";
 import WrappedMap from "./components/location/Map";
+import GpsTable from "./components/location/map-components/gps_page_table";
+import MyTable from "./components/MyTable";
 
 const showToast = (message, type) => {
   console.log("Showing toast...");
@@ -13,7 +15,83 @@ const GPS = () => {
   return (
     <>
       <ToastContainer style={{ zIndex: 9999 }} /> {/* to show above all */}
-      <div className="gps_page" style={{ paddingTop: "58px" }}>
+      <div
+        className="container-fluid"
+        style={{
+          position: "fixed",
+          top: "64px",
+          zIndex: 99999,
+          background: "white",
+        }}
+      >
+        <div className="row">
+          <div
+            class="col-3 d-flex flex-column"
+            style={{
+              border: "2px solid",
+              color: "black",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div>Current location</div>
+          </div>
+
+          <div
+            class="col-3 d-flex flex-column"
+            style={{
+              border: "2px solid",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div>From address</div>
+          </div>
+          <div
+            class="col-3 d-flex flex-column"
+            style={{
+              border: "2px solid",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div>To address</div>
+          </div>
+
+          <div class="col-3" style={{ border: "2px solid", paddingRight: 0 }}>
+            <div className="d-flex justify-content-between">
+              <div style={{alignSelf: "center"}}>
+                Total time
+                {/* Dynamic Content here */}
+              </div>
+              <div class="btn-group">
+                <button
+                  type="button"
+                  class="btn btn-number border border-dark border-2 rounded-0"
+                  data-type="plus"
+                  data-field="quant[2]"
+                  style={{ backgroundColor: "#ff0000", margin: 0 }}
+                >
+                  <i class="fa-solid fa-plus text-dark fa-lg"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-success btn-number border border-dark border-2 rounded-0"
+                  data-type="minus"
+                  data-field="quant[2]"
+                  style={{ backgroundColor: "#39e600", margin: 0 }}
+                >
+                  <i class="fa-solid fa-minus text-dark fa-lg"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{ width: '25%', paddingTop: 90, paddingBottom: 0}} className="table-responsive">
+   <GpsTable  />
+   </div> */}
+      <div className="gps_page">
         <div className="gps_map">
           <WrappedMap
             showToast={showToast}
