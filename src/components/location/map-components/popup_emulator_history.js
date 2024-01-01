cf03a7dc2883661297075344aeac99355f1de7e7
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2024-01-01 16:48:11 +0530

    responsive  PopUpEmulatorHistory modal ui

--- src/components/location/map-components/popup_emulator_history.js
+++ src/components/location/map-components/popup_emulator_history.js
@@ -1,8 +1,7 @@
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import React, { useState} from "react";
+import React, { useState } from "react";
 import PopupEmulatorHistoryTable from "./popup_emulator_history_table";
-
 
 const style = {
   position: "absolute",
@@ -13,21 +12,21 @@ const style = {
   bgcolor: "background.paper",
   boxShadow: "-3px -3px 7px #97949473, 2px 2px 7px rgb(137, 138, 138)",
   pt: 2,
-  px: 4,
-  pb: 3,
+  px: 2,
+  pb: 2,
 };
 
-const PopUpEmulatorHistory = ({
-  showToast,
-  handleClose,
-  open,
-  emulatorHistory,
-}) => {
+const mobileStyle = {
+  width: "90%",
+  maxWidth: "90vw", // Adjust the maximum width for smaller screens
+};
 
+const PopUpEmulatorHistory = ({ showToast, handleClose, open, emulatorHistory }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     handleClose(0);
   };
+
   return (
     <div>
       <Modal
@@ -36,15 +35,25 @@ const PopUpEmulatorHistory = ({
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: "auto ",height:"auto"}}>
-            <h3>HISTORY</h3>
-            <PopupEmulatorHistoryTable
-              data={emulatorHistory}
-              showToast={showToast}
-            />
-            <button className="login_button" type="button" style={{height:"40px"}} onClick={(e) => handleSubmit(e)}>
-              CLOSE
-            </button>
+        <Box
+          sx={{
+            ...style,
+            width: "auto",
+            height: "auto",
+            ...mobileStyle, // Apply mobile styles
+          }}
+        >
+          <h3>HISTORY</h3>
+          {/* Ensure that PopupEmulatorHistoryTable is properly set inside the Box */}
+          <PopupEmulatorHistoryTable data={emulatorHistory} showToast={showToast} />
+          <button
+            className="login_button"
+            type="button"
+            style={{ height: "40px" }}
+            onClick={(e) => handleSubmit(e)}
+          >
+            CLOSE
+          </button>
         </Box>
       </Modal>
     </div>
