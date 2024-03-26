2d305dec1992a7f7660c87573460cf23a9bcd242
Shubham-Ftechiz <shubham.pal@ftechiz.com>
2024-03-26 11:10:25 +0530

    resolve refresh issue of emulator markers

--- src/components/location/map-components/Markers/EmulatorMarkers.jsx
+++ src/components/location/map-components/Markers/EmulatorMarkers.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import useMarkerStore from '../../../../stores/emulator/markerStore.js'
+import { useEmulatorStore } from '../../../../stores/emulator/store.tsx'
 import EmulatorMarker from './EmulatorMarker.jsx'
 
 const EmulatorMarkers = () => {
   const items = useMarkerStore((state) => state.items)
+  const selectedEmulator = useEmulatorStore((state) => state.selectedEmulator)
+
+  const copyEmulator = [...items]
+
+  const indexEmulator = selectedEmulator?.id
+  const index = copyEmulator.indexOf(indexEmulator)
+
+  let itemEmulatorIds
+  if (indexEmulator) {
+    if (index > -1) {
+      copyEmulator.splice(index, 1)
+    }
+    itemEmulatorIds = copyEmulator
+  } else {
+    itemEmulatorIds = items
+  }
 
   return (
     <>
-      {items?.map((id) => {
+      {itemEmulatorIds?.map((id) => {
         return <EmulatorMarker key={id} id={id} />
       })}
     </>
